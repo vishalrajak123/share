a547eb674f0434d149cee6b4fe6fcbaa435e405e
rijusougata13 <rijusougata13@gmail.com>
2019-10-21 16:44:45 +0530

    updated

--- pdftoaudio/pdftoaudio.py
+++ pdftoaudio/pdftoaudio.py
@@ -2,13 +2,14 @@
 import PyPDF2
 from gtts import gTTS
 import os
-from Tkinter import Tk
+from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename()
+print(os.path.basename(filename))
 #create a file obj and open in read binary mode
-pdfFileObj = open('filename','rb')     #'rb' for read binary mode
+pdfFileObj = open(filename,'rb')     #'rb' for read binary mode
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pageObj = pdfReader.getPage(0)          #'9' is the page number
 p=pageObj.extractText()
