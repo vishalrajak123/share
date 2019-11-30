520a85298f5ca726290bcf6cd673853add30217f
rijusougata13 <rijusougata13@gmail.com>
2019-11-30 11:54:52 +0530

    added a webscraper which scrap all the notice from rcciit college website

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -14,27 +14,39 @@ def cipher(string,key):
     for i in range(len(string)):
         if str.islower(string[i]):
             c=(((ord(string[i])-97)+ord(key[i]))%26)+97
-        #c=(ord(string[i])+ord(key[i]))%26
-        #c+=ord('A')
         else:
             if str.isupper(string[i]):
                 c=(((ord(string[i])-65)+ord(key[i]))%26)+65
             else:
-                c=ord(string[i])
-        ciphertext.append(chr(c))
+                if (int(string[i])>=0 and int(string[i])<=9):
+                    c=(((int(string[i])-0)+ord(key[i])%26))%10+0
+                    c=str(c)
+                else:
+                    c=ord(string[i])
+        try:
+            ciphertext.append(chr(c))
+        except:
+            ciphertext.append(c)
     return(''.join(ciphertext))
 def plain(string,key):
     plaintext=[]
     for i in range(len(string)):
         if str.islower(string[i]):
             p=(((ord(string[i])-97)-ord(key[i]))%26)+97
-            #p=(ord("A")-ord(string[i])-ord(key[i]))%26
+
         else:
             if str.isupper(string[i]):
                 p=(((ord(string[i])-65)-ord(key[i]))%26)+65
             else:
-                p=ord(string[i])
-        plaintext.append(chr(p))
+                if (int(string[i])>=0 and int(string[i])<=9):
+                    p=(((int(string[i])-0)-ord(key[i])%26))%10+0
+                    p=str(p)
+                else:
+                    p=ord(string[i])
+        try:
+            plaintext.append(chr(p))
+        except:
+            plaintext.append(p)
     return(''.join(plaintext))
 if __name__ == "__main__":
     q=True
@@ -54,5 +66,3 @@ if __name__ == "__main__":
             print('plaintext: '+plaintext)
         else:
             print('wrong choice')
-        #exit=input("to quit press 1\nto continue press other number")
-
